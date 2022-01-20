391557ace19d899ffdfafeeb8b75938a662ed664
Akshay Ugale <uakshay@lentra.ai>
2022-01-20 14:47:15 +0530

    added dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,7 +2,13 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
+
+	public int countWords(String words) {
+		String [] separateWords = StringUtils.split(words,' ');
+		return (separateWords == null)? 0 : separateWords.length;
+	}
 	
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
@@ -20,7 +26,9 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-		Application app = new Application();
-		app.greet();
+	Application app = new Application();
+	app.greet();
+	int count =app.countWords("I have four words");
+	System.out.println("Word Count: "+count);
     }
 }
